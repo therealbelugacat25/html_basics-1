529bd5e4433a57684a13ed8f09b87734c1028b6d
NightlyD3V <TylerSpaulding95@gmail.com>
2022-10-14 20:19:53 -0400

    Added mouseover effects to images

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello_World");
+
+const images = document.getElementsByName("images");
+console.log(images)
+const imageHover = function(){
+    const images_arr = Array.from(images);
+    images_arr.map((element) => {
+        element.addEventListener("mouseover", function(){element.style.margin = "0px";});
+        element.addEventListener("mouseleave", function(){element.style.margin = "25px";})
+    })
+}
+
+imageHover();
